0a81ae9dcba15ef10d39fe757ee2bac5c5fd2a40
yangyang <fuxiaofeng1107@163.com>
2017-08-12 12:02:09 +0800

    feign eureka riddon

--- springcloud-client/src/main/java/com/client/controller/ClientApp.java
+++ springcloud-client/src/main/java/com/client/controller/ClientApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
@@ -13,6 +14,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class ClientApp {
 
     public static void main(String[] args) {
--- springcloud-client/src/main/java/com/client/controller/ServerController.java
+++ springcloud-client/src/main/java/com/client/controller/ServerController.java
@@ -1,5 +1,6 @@
 package com.client.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,14 @@ public class ServerController {
     FeignClientController feignClientController;
 
     @GetMapping("c1")
+    @HystrixCommand(fallbackMethod = "serverDead")
     public String c1(){
         System.out.println("feignClientController.....");
         return feignClientController.testeur();
     }
 
+    public String serverDead(){
+        System.out.println("serverDead.......");
+        return "serverDead....";
+    }
 }
