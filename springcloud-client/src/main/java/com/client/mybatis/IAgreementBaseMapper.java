8c010570fc40b0b5d22297da4148ef8753a344f5
xiaofengfu <fuxiaofeng1107@163.com>
2018-03-05 21:29:13 +0800

    annotation mapper

---
+++ springcloud-client/src/main/java/com/client/mybatis/IAgreementBaseMapper.java
@@ -0,0 +1,34 @@
+package com.client.mybatis;
+
+import com.client.mybatis.domain.AgreementBase;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface IAgreementBaseMapper {
+
+
+	@Select("select * from roles where user_id=#{userId}")
+	@Results(
+			{
+					@Result(id = true, column = "role_id", property = "roleId"),
+					@Result(column = "rolename", property = "rolename"),
+					@Result(column = "privilege", property = "privilege_id"),
+					@Result(column = "description",property = "description")
+			})
+	List<AgreementBase> findByAgreementBaseId(int userId);
+
+
+	@Select("select * from agreement_base")
+	@Results(
+	{
+	@Result(id = true, column = "user_id", property = "userId"),
+	@Result(column = "name", property = "name"),
+	@Result(column = "email", property = "email"),
+	@Result(property = "roles", column = "user_id",
+			many = @Many(select = "com.client.mybatis.AgreementBaseMapper.findByAgreementBaseId"))
+	})
+	public List<AgreementBase> findAll();
+
+}
